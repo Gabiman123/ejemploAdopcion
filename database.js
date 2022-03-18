6ed15eb242e03e08f42f24f7384c5b6119f09726
Gabriel Ibarra <toro270294@gmail.com>
2022-03-18 07:32:07 -0600

    Update database.js

--- database.js
+++ database.js
@@ -1,9 +1,9 @@
 let config = {
-	host: "localhost",
-	user: "root",
-	password: "",
+	host: "us-cdbr-east-05.cleardb.net",
+	user: "bd7a355ad57b37",
+	password: "431da127",
 	port: 3306,
-	database: "adoption_system",
+	database: "heroku_60b0a2e005c51d8",
 	secret_key: "k6Oj9d9xZywqpPje"
 };
 exports.config = config;
